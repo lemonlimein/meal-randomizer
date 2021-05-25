5dc28b81bb206f935fde82d5fec70dbc7b77c349
∆∆∆ <work@Macintosh.local>
2021-05-24 22:06:37 -0400

    redirect pages

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
-chrome.runtime.onMessage.addListener(redirect);
-
-function redirect(request, sender, sendResponse) {
-  chrome.tabs.update({ url: request.message});
-}
+chrome.runtime.onMessage.addListener(function (request) {
+  if (request.message == 'start') {
+    chrome.tabs.create({ url: request.link });
+    console.log('tab created...');
+    chrome.tabs.executeScript({
+      code: 'runFlag = true;'
+    });
+  }
+  if(request.message == 'change'){
+    chrome.tabs.create({ url: request.link });
+    console.log('tab changed...');
+    chrome.tabs.executeScript({
+      code: 'runFlag = true;'
+    });
+  }
+});
--- popup.js
+++ popup.js
@@ -1,23 +1,44 @@
-var link = "https://www.seriouseats.com/";
-
-
-function popup() {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": "start" });
-    });
+function breakfastRun() {
+    chrome.runtime.sendMessage({ 'message': 'start', "link": "https://www.seriouseats.com/breakfast-brunch-recipes-5117905" });
 }
+
+function mainsRun(){
+    chrome.runtime.sendMessage({ 'message': 'start', "link": "https://www.seriouseats.com/main-recipes-5117839" });
+}
+
+function snacksRun(){
+    chrome.runtime.sendMessage({ 'message': 'start', "link": "https://www.seriouseats.com/snack-appetizer-recipes-5117792" });
+}
+
+function saladsRun(){
+    chrome.runtime.sendMessage({ 'message': 'start', "link": "https://www.seriouseats.com/salad-recipes-5117809" });
+}
+
+function sidesRun(){
+    chrome.runtime.sendMessage({ 'message': 'start', "link": "https://www.seriouseats.com/side-recipes-5117798" });
+}
+
+function dessertsRun(){
+    chrome.runtime.sendMessage({ 'message': 'start', "link": "https://www.seriouseats.com/dessert-recipes-5117878" });
+}
+
+function drinksRun(){
+    chrome.runtime.sendMessage({ 'message': 'start', "link": "https://www.seriouseats.com/drink-recipes-5117861" });
+}
+
+function condimentsRun(){
+    chrome.runtime.sendMessage({ 'message': 'start', "link": "https://www.seriouseats.com/condiment-sauce-recipes-5117888" });
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("appTitle").addEventListener("click", redirect);
-    // link = document.getElementById("Breakfast").value;
     // document.getElementById("Breakfast").addEventListener("click", redirect);
-    document.getElementById("Breakfast").addEventListener("click", popup);
+    document.getElementById("breakfast").addEventListener("click", breakfastRun);
+    document.getElementById("mains").addEventListener("click", mainsRun);
+    document.getElementById("snacks").addEventListener("click", snacksRun);
+    document.getElementById("salads").addEventListener("click", saladsRun);
+    document.getElementById("sides").addEventListener("click", sidesRun);
+    document.getElementById("desserts").addEventListener("click", dessertsRun);
+    document.getElementById("drinks").addEventListener("click", drinksRun);
+    document.getElementById("condiments").addEventListener("click", condimentsRun);
 });
-
-
-// 
-
-function redirect() {
-    chrome.runtime.sendMessage({ "message": link});
-}
